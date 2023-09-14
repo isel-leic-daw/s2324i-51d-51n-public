d91391a7767db1a7a8ddb21f5772e8c15a3fbf2c
Pedro Felix <pmhsfelix@gmail.com>
2023-09-14 08:44:34 +0100

    adds note about servlets

--- code/jvm/servlet-intro/src/main/kotlin/pt/isel/daw/servlets/Servlets.kt
+++ code/jvm/servlet-intro/src/main/kotlin/pt/isel/daw/servlets/Servlets.kt
@@ -14,18 +14,24 @@ import org.slf4j.LoggerFactory
  * They are now maintained by project [Jakarta](https://projects.eclipse.org/projects/ee4j)
  *
  */
+
+/**
+ * A servlet is responsible for ultimately handling an HTTP request by populating an [HttpServletResponse]
+ * using information from a [HttpServletRequest].
+ */
 class ExampleServet : HttpServlet() {
 
     override fun doGet(
         request: HttpServletRequest,
         response: HttpServletResponse,
-    ) : Unit {
+    ) {
         log.info("doGet: method='{}', uri='{}'",
             request.method,
             request.requestURI)
 
         response.status = 200
         // QUESTION what happens if 'charset=utf-8' is removed
+        // QUESTION what happens if 'Content-Type' is set to 'application/json'
         response.addHeader("Content-Type", "text/plain; charset=utf-8")
         response.outputStream.apply {
             write("Olá mundo".toByteArray(Charsets.UTF_8))
@@ -39,14 +45,27 @@ class ExampleServet : HttpServlet() {
     }
 }
 
+/**
+ * A servlet filter contributes to the handling of HTTP responses, by using [HttpServletRequest]
+ * and eventually mutating [HttpServletResponse] *before* and *after* the request is handled by a server.
+ * Multiple filters are organized in a pipeline.
+ */
+
 class ExampleFilter : HttpFilter() {
     override fun doFilter(
         request: HttpServletRequest,
         response: HttpServletResponse,
         chain: FilterChain) {
+
+        // Performed before the request is handled by the servlet
         val start = System.nanoTime()
         log.info("doFilter: before chain call")
+
+        // Call the rest of the pipeline
+        // QUESTION what happens if a filter does not call chain.doFilter?
         chain.doFilter(request, response)
+
+        // Performed after the request was handled by the servlet
         val end = System.nanoTime()
         val delta = end - start
         log.info("doFilter: after chain call, took: {} us", delta)
